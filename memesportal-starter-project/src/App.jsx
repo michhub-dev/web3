a840580b560c9ff299cafe7d78e78399129e71d1
Michelle David <57120467+michhub-dev@users.noreply.github.com>
2022-04-20 17:08:08 +0100

    Updated code

--- memesportal-starter-project/src/App.jsx
+++ memesportal-starter-project/src/App.jsx
@@ -1,17 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "./App.css";
-import abi from "./utils/memeContract.json";
+import memeContract from "./utils/memeContract.json";
 
 const App = () => {
   // set useState to store users wallet
   const [usersWallet, setUsersWallet] = useState("");
 
+  // to store all memes
+  const [allMemes, setAllMemes] = useState([]);
+  
   // store the contract address in a variable 
   const contractAddress = "0xa8Db68b275bB9bA8b73339f5a82548320F10A1d3";
   
     // Variable that reference the abi content 
-  const contractABI = abi.abi; // ABI file is something our web app needs to know how to communicate with our contract
+ // const contractABI = abi.abi; // ABI file is something our web app needs to know how to communicate with our contract
+
+const getAllMemes = async () => {
+    try{
+      const { ethereum } = window; 
+
+      if (window.ethereum) {
+ const provider = new ethers.providers.Web3Provider();
+            const signer = provider.getSigner();
+        
+        const memePortalContract = new ethers.contract(contractAddress, memeContract.abi, signer);
+
+        const memes = memePortalContract.getAllMemes();
+
+        // I only want address, timestamp, and message in my ui
+        const cleanedMeme = memes.map( mem => {
+          return {
+            address: mem.sender,
+            timestamp: new Date (mem.timestamp * 100),
+            message: mem.message,
+          };
+        });
+   
+            // store the data in react state
+            setAllMemes(cleanedMeme);
+       
+      
+      } else {
+        console.log("ethereum object doesn't exist")
+      }
+      } catch(error) {
+    console.log(error);
+    } 
+  }
   
   const isWalletConnected = async () => {
     try {
@@ -34,6 +70,7 @@ use this method eth_accounts to see if we're authorized to access any of the acc
         const account = accounts[0];
         console.log("Found an authorize account:",  account);
         setUsersWallet(account)
+        //getAllMemes()
       } else {
         console.log("No authorized account found")
       }
@@ -77,7 +114,7 @@ use eth_requestAccounts function to ask Metamask to give me access to the user's
         * Execute the actual meme from the smart contract
         */
         
-        const memeTxn = await memePortalContract.meme(message, { gasLimit: 300000}); 
+        const memeTxn = await memePortalContract.meme(message, { gasLimit: 300000});
         console.log("Mining...", memeTxn.hash);
 
         await memeTxn.wait();
@@ -88,20 +125,49 @@ use eth_requestAccounts function to ask Metamask to give me access to the user's
         console.log("Retrieved total meme count...", count.toNumber());
         
       } else {
-        console.log("ethereum object doesn't exist")
+        console.log("ethereum object doesn't exist");
       }
     } catch (error) {
       console.log(error)
     }
   }
+
+  
   /*
   * this runs our function when the page loads
   */
   useEffect(() => {
-    isWalletConnected();
-  }, [])
+    let memePortalContract;
+   isWalletConnected();
+    
+    const oneNewMeme = (from, timestamp, message) =>{
+      console.log("newMeme", from, timestamp, message);
+      setAllMemes(prevState => [
+        ...prevState, 
+        {
+          address: mem.sender,
+            timestamp: new Date (mem.timestamp * 100),
+            message: mem.message,
+        },
+      ]);
+    };
+    if (window.ethereum) {
+      const provider = new ethers.providers.web3provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      memePortalContract = new ethers.contract(contractAddress, contractABI, signer);
+      memePortalContract.on("NewMeme", oneNewMeme);
+    }
+    return () => {
+      if (memePortalContract) {
+        memePortalContract.off("NewMeme", oneNewMeme)
+      }
+    };
+ 
+  }, []);
 
   return (
+    
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
@@ -124,9 +190,20 @@ use eth_requestAccounts function to ask Metamask to give me access to the user's
         * If there is no currentAccount render this button
         */}
         
-        {!usersWallet && (
+         {!usersWallet && (
       <button className="memeButton" onClick={connectWallet}> Connect me!</button>
       )}
+        
+         {getAllMemes.map((mem, index) => {
+          return (
+        <div key={index} style={{ backgroundColor: "oldLace", marginTop: "16px", padding: "8px" }}>
+        
+           <div> Address: {mem.sender} </div>
+          <div> Time: {mem.timestamp.toString()} </div>
+          <div> Message: {mem.message} </div>
+        </div>)
+          
+        })}
       </div>
     </div>
   );
@@ -134,11 +211,11 @@ use eth_requestAccounts function to ask Metamask to give me access to the user's
 
 export default App
 
-/*
+
 
 
 
 
 
  
-  */
+  
